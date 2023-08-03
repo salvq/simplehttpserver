414e1a528a12264fc1c2e929c56819ceb39be751
salvq <43242348+salvq@users.noreply.github.com>
2023-08-03 09:18:29 +0200

    Update simple_server.py

--- script/simple_server.py
+++ script/simple_server.py
@@ -2,6 +2,8 @@ import http.server
 import socketserver
 import os
 
+print(f'HTTP server is starting up')
+
 folder = '/simplehttpserver'
 os.chdir(folder)
 
@@ -9,8 +11,9 @@ Handler = http.server.SimpleHTTPRequestHandler
 httpd = socketserver.TCPServer(("", 8000), Handler)
 
 try:
+    print(f'HTTP server is running')
     httpd.serve_forever()
 
 except Exception as e:
     httpd.server_close()
-    print(f'{current_time} Errors in program: {e}')
+    print(f'Errors in program: {e}')
